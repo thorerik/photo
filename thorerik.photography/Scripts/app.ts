0093b710f133f3d80d5b5305282c5fe8ae79063c
Thor Erik Lie <thor@thorerik.com>
2014-05-19 00:17:43 +0200

    Minor css adjustments and start of Javascript

--- thorerik.photography/Scripts/app.ts
+++ thorerik.photography/Scripts/app.ts
@@ -3,6 +3,25 @@
 
 
 window.onload = () => {
-    var el = document.getElementById('content');
-    
+    var sidebarcollapsed = false;
+    $('#showhide').click(function () {
+        if (sidebarcollapsed) {
+            $('#sidebar').animate({
+                right: "+=320"
+            }, 500, function () {
+                $('#showhide').html("»");
+                sidebarcollapsed = false;
+                $('#content').width("100%");
+            });
+        }
+        else {
+            $('#sidebar').animate({
+                right: "-=320"
+            }, 500, function () {
+                $('#showhide').html("«");
+                sidebarcollapsed = true;
+                $('#content').width("75%");
+                });
+        }
+    });
 };
